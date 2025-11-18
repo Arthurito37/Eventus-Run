222f7965af25dc7def213c618c28fc992f1adb6c
Arthurito37 <arthursantana364@outlook.com>
2025-11-17 21:47:07 -0300

    JS e Body

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+    alert("Ola mundo"); // Exibe uma caixa de alerta no navegador com a mensagem
+    console.log("Esta funcionando")
+    let a, b, c, d, e; //Declara três váriavei
+    a = "Corrida";
+    b = "Ciclismo";
+    c = "Crossfit";
+    d = "Natacao";
+    e = "Triatlhon";
+    console.log(a, b, c, d, c)
